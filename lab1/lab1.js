1eb95444de6090eb2df73b999787773219a98b04
Ha Nguyen <hnk6@duke.edu>
2026-08-27 22:25:42 +0800

    add async

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,50 +1,53 @@
-const width = 800;
-const height = 450;
-const chartHeight = 350;
+async function scatterPlot() {
+    const width = 800;
+    const height = 450;
+    const chartHeight = 350;
 
-/* Meaning of codeblock below:
-<div id="chart">
-     <svg width="700" height="400"></svg>
- </div> */
-const svg = d3.select('#chart')
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    /* Meaning of codeblock below:
+    <div id="chart">
+        <svg width="700" height="400"></svg>
+    </div> */
+    const svg = d3.select('#chart')
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-d3.csv("../data/students.csv", d => ({
-    name: d.name,
-    score: +d.score
-})).then(data => {
-    console.log(data);
+    d3.csv("../data/students.csv", d => ({
+        name: d.name,
+        score: +d.score
+    })).then(data => {
+        console.log(data);
 
-    //Bars
-    svg.selectAll("rect")
-        .data(data)
-        .join("rect")
-        .attr("x", (d,i) => i*90+20)
-        .attr("y", d => chartHeight - d.score*3 )
-        .attr("width", 60)
-        .attr("height", d => d.score*3)
-        .attr("fill", "Violet");
+        //Bars
+        svg.selectAll("rect")
+            .data(data)
+            .join("rect")
+            .attr("x", (d,i) => i*90+20)
+            .attr("y", d => chartHeight - d.score*3 )
+            .attr("width", 60)
+            .attr("height", d => d.score*3)
+            .attr("fill", "Violet");
 
-    //Student Names
-    svg.selectAll(".name")
-        .data(data)
-        .join("text")
-        .attr("class", "name")
-        .attr("x", (d,i) => i*90+50)
-        .attr("y", chartHeight + 25)
-        .attr("text-anchor", "middle")
-        .text(d=> d.name);
+        //Student Names
+        svg.selectAll(".name")
+            .data(data)
+            .join("text")
+            .attr("class", "name")
+            .attr("x", (d,i) => i*90+50)
+            .attr("y", chartHeight + 25)
+            .attr("text-anchor", "middle")
+            .text(d=> d.name);
 
-    //Scores
-    svg.selectAll(".score")
-        .data(data)
-        .join("text")
-        .attr("class", "score")
-        .attr("x", (d,i) => i*90+50)
-        .attr("y", chartHeight + 45)
-        .attr("text-anchor", "middle")
-        .text(d => d.score)
-});
+        //Scores
+        svg.selectAll(".score")
+            .data(data)
+            .join("text")
+            .attr("class", "score")
+            .attr("x", (d,i) => i*90+50)
+            .attr("y", chartHeight + 45)
+            .attr("text-anchor", "middle")
+            .text(d => d.score)
+    });
+}
 
+scatterPlot()
